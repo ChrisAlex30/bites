b0abbe0ac35db5a81a6e371a7a1226f92e21be93
chrisalex30 <akshara155301@gmail.com>
2026-03-20 11:16:21 +0530

    Step 4: Redis Setup

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { validate } from '../middlewares/validate.js';
 import { restaurantSchema, type Restaurant } from '../schemas/restaurant.js';
+import { getRedisClient } from '../utils/client.js';
 const router = express.Router();
 
-router.post('/', validate(restaurantSchema),(req, res) => {
+router.post('/', validate(restaurantSchema),async (req, res) => {
   const data=req.body as Restaurant;
+  const client=await getRedisClient();
   res.send(`List of restaurants with name ${data.name}`);
 });
 
